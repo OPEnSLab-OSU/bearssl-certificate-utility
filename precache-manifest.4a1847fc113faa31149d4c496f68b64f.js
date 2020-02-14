24d5f7529f1e2872fc7637f23a6fe0870deb447d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-14 19:46:17 +0000

    Deploy OPEnSLab-OSU/bearssl-certificate-utility to github.com/OPEnSLab-OSU/bearssl-certificate-utility.git:gh-pages

---
+++ precache-manifest.4a1847fc113faa31149d4c496f68b64f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9fd5b14a97b5cd3116d778cb76f522f",
+    "url": "/bearssl-certificate-utility/index.html"
+  },
+  {
+    "revision": "b5741f3b9493cefe6609",
+    "url": "/bearssl-certificate-utility/static/css/main.0b79f4d3.chunk.css"
+  },
+  {
+    "revision": "69cb3f359db9486ef167",
+    "url": "/bearssl-certificate-utility/static/js/2.b7df4f61.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bearssl-certificate-utility/static/js/2.b7df4f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5741f3b9493cefe6609",
+    "url": "/bearssl-certificate-utility/static/js/main.251d4f37.chunk.js"
+  },
+  {
+    "revision": "9bde829618c46b11eca4",
+    "url": "/bearssl-certificate-utility/static/js/runtime-main.0830f59f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bearssl-certificate-utility/precache-manifest.66970d1e880307487e2552e1a22b9e37.js"
+  "/bearssl-certificate-utility/precache-manifest.4a1847fc113faa31149d4c496f68b64f.js"
 );
 
 self.addEventListener('message', (event) => {
